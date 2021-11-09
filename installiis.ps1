0440c73ad7a6acee02e8175bc54128ae979f1326
rchenault <30476212+rchenault@users.noreply.github.com>
2021-11-09 11:44:21 -0500

    Update installiis.ps1

--- installiis.ps1
+++ installiis.ps1
@@ -1,2 +1,3 @@
 Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools
+mkdir c:\inetpub\testsite
 New-IISSite -Name "TestSite" -BindingInformation "*:80:" -PhysicalPath "$env:systemdrive\inetpub\testsite"

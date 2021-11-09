dc6f771e409c034dea1176531a381497a6dbf4cc
rchenault <30476212+rchenault@users.noreply.github.com>
2021-11-09 09:33:34 -0500

    Update installiis.ps1

--- installiis.ps1
+++ installiis.ps1
@@ -1,2 +1,2 @@
-"commandToExecute": "powershell -ExecutionPolicy Unrestricted Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools"
-"commandToExecute": "powershell -ExecutionPolicy Unrestricted New-IISSite -Name "TestSite" -BindingInformation "*:80:" -PhysicalPath "$env:systemdrive\inetpub\testsite""
+Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools
+New-IISSite -Name "TestSite" -BindingInformation "*:80:" -PhysicalPath "$env:systemdrive\inetpub\testsite"
